ec69ce1d264aaa5ca4d314250db59481ff391f98
Astrale_works <ash4567@outlook.fr>
2023-10-16 12:18:35 +0200

    css + basic app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [todos,setTodos] = useState({
+    "my cool list": []
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root" className="">
+      <div className="hbox expandX center space-around">
+        <div style={{
+          padding: 15,
+          borderRadius: 10,
+          backgroundColor: "rgb(179, 154, 154)",
+        }}>TODO APP
+        </div>
+      </div>
+
+      <div className="hbox expandXY">
+        {/* <!-- List selectors --> */}
+        <div className="vbox expandY">
+          <div className="center expandX box"
+            style={{
+              border: "1px black solid",
+            }}>
+            <div>Todo lists</div>
+          </div>
+          <div
+            className="vbox expandY children-elem"
+            style={{
+              minWidth: 300, minHeight: 700
+            }}>
+            <div></div>
+          </div>
+        </div>
+        {/* <!-- Here are the todo lists --> */}
+        <div className="vbox expandY">
+          <div className="center expandX box"
+            style={{ border: "1px black solid;" }}>
+            <div>My cool list</div>
+          </div>
+
+          <div className="vbox expandY children-elem"
+            style={{
+              minWidth: 300,
+              minHeight: 700,
+            }}>
+            <div></div>
+          </div>
+        </div>
+      </div>
     </div>
+
+
   );
 }
 
