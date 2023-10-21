a628935eec66171e926744f7c839a486b7438067
Astrale <ash4567@outlook.fr>
2023-10-21 17:11:38 +0200

    server update

--- server/server.ts
+++ server/server.ts
@@ -1,35 +1,32 @@
-const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');const express = require('express');
-const path = require('path');
-const cors = require('cors')
-const PORT = process.env.PORT || 8000;
+const path = require('path')
+import express from 'express'
+import cors from 'cors'
+import { Data } from '../src/interface'
+
+const PORT = process.env.PORT || 8000
 const app = express();
 
+
 app.use(cors())
-app.use(expressCspHeader({
-  directives: {
-      'default-src': [SELF],
-      'script-src': [SELF, INLINE, 'somehost.com'],
-      'style-src': [SELF, 'mystyles.net'],
-      'img-src': ['data:', 'images.com'],
-      'worker-src': [NONE],
-      'block-all-mixed-content': true
-  }
-}));
+app.use(express.json());
 
 
-// let todos = { "My great todo list": [] }
+let data : Data = new Data()
+data.addList("My server list")
 
 
+console.log("init server... listening to PORT:", PORT)
 
+app.use(express.static(path.join(__dirname, '../build/')));
 
-console.log("init server...")
-// console.log(express.static(path.join(__dirname, 'build')))
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/backend', function (req, res) {
-  console.log("accessed backend")
-  // res.set("Content-Security-Policy", "default-src 'self'");
-  // res.sendFile(path.join(__dirname, '../build', './index.html'));
+app.get("/getdata", (req, res) => {
+  console.log("at /getdata ... Sending")
+  res.send(data.__ToJSON())
 });
 
-app.listen(PORT);
+app.post("/setdata", (req, res) => {
+  console.log("at setdata ... Receiving")
+  data = data.__FromJSON(JSON.stringify(req.body))
+})
+
+app.listen(PORT)
